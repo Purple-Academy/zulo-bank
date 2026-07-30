74ffc28856c883bfafd9326635e194ec9e6054df
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2026-07-30 10:44:28 +0400

    (refactor): cleaned up Welcome component

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -2,7 +2,7 @@ export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-       
+       <h1>Hello World</h1>
       </div>
     </main>
   );
