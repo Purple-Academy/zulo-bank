1defb16dc4663321577d07fc5e717883f85b1f60
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2026-09-10 10:45:58 +0400

    feat: add navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <header className="w-full h-20 bg-white text-black flex justify-center items-center">
+      <nav className="w-ful h-full gap-5 flex justify-center items-center">
+        <p>Main</p>
+        <p>About</p>
+        <p>Contact</p>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
